80665a2d6db4a3e3b731a000759ca231b6c352e8
prasad <prasad.kuri95@gmail.com>
2022-08-16 09:41:30 +0530

    preloading

--- secrettranslator/src/App.js
+++ secrettranslator/src/App.js
@@ -1,26 +1,39 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Admin from "./Admin";
+import "./App.css";
+import Login from "./components/Login";
+import Main from "./components/Main";
+import Selector from "./components/Selector";
+import UploadFile from "./components/UploadFile";
+import Animation from "./voice.gif";
 
-import {BrowserRouter as Router , Routes,Route} from 'react-router-dom';
-import Login from './components/Login';
-import Selector from './components/Selector';
-import UploadFile from './components/UploadFile';
-import Main from './components/Main';
-import Admin from './Admin';
 function App() {
-  return (
-    <div>
-         <Router>
-           <Routes>
-             <Route path='/' exact element={<Login/>} />
-             <Route path='/selector' exact element={<Selector/>} />
-             <Route path='/upload' exact element={<UploadFile/>} />
-             <Route path='/main' exact element={<Main/>} />
-             <Route path='/admin' exact element={<Admin />} />
+  const [loading, setloading] = useState(false);
 
-           </Routes>
-         </Router>
-        
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <img className="voiceImg" src={Animation} alt="hey" />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<Login />} />
+            <Route path="/selector" exact element={<Selector />} />
+            <Route path="/upload" exact element={<UploadFile />} />
+            <Route path="/main" exact element={<Main />} />
+            <Route path="/admin" exact element={<Admin />} />
+          </Routes>
+        </Router>
+      )}
     </div>
-   
   );
 }
 
--- secrettranslator/src/components/Login.js
+++ secrettranslator/src/components/Login.js
@@ -1,64 +1,55 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { useState } from "react";
 
-import {useNavigate} from 'react-router-dom';
-import { useState } from 'react';
-
+import { useNavigate } from "react-router-dom";
 /** local imports */
-import { loginApi } from './services/api';
-import { getUser } from './services/api';
-
-
-
-const initialValues={
-  userName:"",
-  password:"",
-  
+import { getUser, loginApi } from "./services/api";
+const initialValues = {
+  userName: "",
+  password: "",
 };
 
 const theme = createTheme();
 
 export default function Login() {
-
   const navigate = useNavigate(); // initializing useNavigate for redirections
-  const [user,setUser]=useState(initialValues);
+  const [user, setUser] = useState(initialValues);
 
-  const onValueChange=(e)=>{
-    setUser({...user,[e.target.name]:e.target.value})
-    console.log(e.target.value)
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(e.target.value);
     console.log(user);
-   }
+  };
 
-   const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault(); //vvvvvvvvvvvvvvvvvvvvvviiiiiiiiiiiiiiiiiiiiiiimmmmmmmmmmmmmmmmmmmmmmmmmmm (very imp)
-    loginApi(user).then((res)=>{
-      const data=res.data
-      if(data.msg==="success"){
+    loginApi(user).then((res) => {
+      const data = res.data;
+      if (data.msg === "success") {
         navigate("/selector");
-      }
-      else{
+      } else {
         alert("Invalid username or password!!");
       }
-    })
+    });
     // getUser().then((res)=>{
     //     const data=res.data;
     //     console.log(data.msg)
     // })
-   }
+  };
 
-  
   // const handleSubmit = (event) => {
   //   event.preventDefault();
   //   const data = new FormData(event.currentTarget);
@@ -68,29 +59,30 @@ export default function Login() {
   //   });
   // };
 
-
-
   return (
-
-    
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -100,7 +92,7 @@ export default function Login() {
               name="userName"
               autoComplete="email"
               autoFocus
-              onChange={(e)=>onValueChange(e)}
+              onChange={(e) => onValueChange(e)}
             />
             <TextField
               margin="normal"
@@ -111,7 +103,7 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
-              onChange={(e)=>onValueChange(e)}
+              onChange={(e) => onValueChange(e)}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -131,9 +123,7 @@ export default function Login() {
                   Forgot password?
                 </Link>
               </Grid>
-              <Grid item>
-                
-              </Grid>
+              <Grid item></Grid>
             </Grid>
           </Box>
         </Box>
@@ -145,13 +135,18 @@ export default function Login() {
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Secret Translator
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
